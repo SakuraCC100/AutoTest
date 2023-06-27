cad3abec1f2ba12f7d07c398b1c465f5319c7652
SakuraCC10 <919023162@qq.com>
2023-06-27 15:50:08 +0800

    第一次提交

---
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,13 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+
+    //最基本注解，将方法标记为测试的一部分
+    @Test
+    public void testCase1(){
+        System.out.println("这是第一个测试用例");
+    }
+
+}
