97decb6fcf5a55399ff77b0311b41fe1b59089eb
SakuraCC10 <919023162@qq.com>
2023-07-04 16:11:00 +0800

    testng测试

--- Chapter5/src/main/java/com/sakura/testng/ExpectedException.java
+++ Chapter5/src/main/java/com/sakura/testng/ExpectedException.java
@@ -20,7 +20,7 @@ public class ExpectedException {
     //这是一个测试结果会成功的异常测试
     @Test(expectedExceptions = RuntimeException.class)
     public void runTimeExceptionSuccess(){
-        System.out.println("这是我的异常测试");
+        System.out.println("这是一个成功的异常测试");
         throw new RuntimeException();
     }
 }
